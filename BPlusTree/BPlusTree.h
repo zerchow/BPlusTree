1748186bff163b3432105cf7b7bdac52a5465046
zerchow <455063040@qq.com>
2014-10-19 21:52:16 +0800

    yy

--- BPlusTree/BPlusTree.h
+++ BPlusTree/BPlusTree.h
@@ -3,30 +3,21 @@
 
 using namespace std;
 
-class BPlusTreeNode//内部节点;
+#define M 5
+//在内存中建立一个数据文件，文件大小必须在500M以上，具体数据项自行决定，项数20M以上;
+struct  BPlusTreeNode//B+树节点;
 {
-private:
-public:
-	struct IndexUnit          //索引信息结构;
-	{
-		int index;            //索引;
-		BPlusTreeNode * child;//索引指向的节点;
-	};
-	int indexnum;          //索引数目;
-	list<IndexUnit> indexs;//节点上的索引信息;
-	BPlusTreeNode * parent;//父母节点;
-	int childnum;          //是父母的第几个孩子;
-	bool isLeaf;           //是否为叶子节点;
+	int indexnum;                   //索引数目;
+	int indexs[M];                  //索引;
+	void * pointers[M + 1];         //子节点指针;
+	BPlusTreeNode * parent;         //父母节点;
+	int childnum;                   //是父母的第几个孩子;
+	bool isLeaf;                    //是否为叶子节点;
 };
 
-class BPlusTreeLeaf : BPlusTreeNode//叶子节点;
+struct Data
 {
-/*
-* 增加数据内容;
-*/
-private:
-	list<int> datas;
-public:
+	Data * next;
 };
 
 class BPlusTree
