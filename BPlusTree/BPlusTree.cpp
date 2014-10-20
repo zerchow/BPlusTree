a46df3668f29aed52b74c7caf01b6bbb4001df64
zerchow <455063040@qq.com>
2014-10-21 01:00:35 +0800

    遗憾至极

--- BPlusTree/BPlusTree.cpp
+++ BPlusTree/BPlusTree.cpp
@@ -3,22 +3,61 @@
 int main()
 {
 	BPlusTree * tree = new BPlusTree();
-	tree->insert({ 10, "" });
-	tree->insert({ 11, "" });
-	tree->insert({ 12, "" });
-	tree->insert({ 13, "" });
-	tree->insert({ 14, "" });
-	tree->insert({ 15, "" });
+	Data test1 = {10,""};
+	Data test2 = {11,""};
+	Data test3 = {12,""};
+	Data test4 = {13,""};
+	Data test5 = {14,""};
+	Data test6 = {15,""};
+	Data test7 = {16,""};
+	Data test8 = {17,""};
+	Data test9 = {18,""};
+	Data test10 = {19,""};
+	Data test11 = {20,""};
+	Data test12 = {21,""};
+	Data test13 = {22,""};
+	Data test14 = {23,""};
+	Data test15 = {24,""};
+	Data test16 = {25,""};
+	Data test17 = {26,""};
+	Data test18 = {27,""};
+	tree->insert(test1);
+	tree->insert(test2);
+	tree->insert(test3);
+	tree->insert(test4);
+	tree->insert(test5);
+	tree->insert(test6);
+	tree->insert(test7);
+	tree->insert(test8);
+	tree->insert(test9);
+	tree->insert(test10);
+	tree->insert(test11);
+	tree->insert(test12);
+	tree->insert(test13);
+	tree->insert(test14);
+	tree->insert(test15);
+	tree->insert(test16);
+	tree->insert(test17);
+	tree->insert(test18);
 	BPlusTreeNode * root = tree->getroot();
 	int rootnum = root->index_num;
 	for (int i = 0; i < rootnum; i++)
 		cout << root->indexs[i] << " ";
 	cout << endl;
-	BPlusTreeData * leaf = (BPlusTreeData*)root->pointers[0];
+	for(int i = 0; i < rootnum; i ++)
+	{
+		BPlusTreeNode * node = (BPlusTreeNode*)root->pointers[i];
+		for (int j = 0; j < node->index_num; j ++)
+		{
+			cout<< node->indexs[j]<<" ";
+		}
+		cout<<endl;
+	}
+	/*BPlusTreeData * leaf = (BPlusTreeData*)root->pointers[0];
 	while (leaf)
 	{
 		cout << leaf->index_num << endl;
 		leaf = leaf->next;
-	}
+	}*/
 	return 0;
 }
