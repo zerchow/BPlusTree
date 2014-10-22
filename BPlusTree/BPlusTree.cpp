6c9612a486682b731b7103a9579b3124df7f6725
zerchow <455063040@qq.com>
2014-10-22 11:04:50 +0800

    ...

--- BPlusTree/BPlusTree.cpp
+++ BPlusTree/BPlusTree.cpp
@@ -1,20 +1,30 @@
 #include "BPlusTree.h"
 int main()
 {
-	Data test[27]; //32 * 27 = 864 (KB);
-	for (int i = 0; i < 27; i++)
+	BPlusTreeData leafleaf;
+	BPlusTreeNode nodenode;
+	cout<<sizeof(leafleaf)<<endl;
+	cout<<sizeof(nodenode)<<endl;
+	return 0;
+	Data * test = new Data[16384000]; //32 * 27 = 864 (B);
+	
+	for (int i = 0; i < 16384000; i++)
 	{
 		test[i].index = 10 + i;
 		strcpy(test[i].data, "test");
 	}
+	
 	BPlusTree * tree = new BPlusTree();
-	for (int i = 0; i < 27; i++)
+	for (int i = 0; i < 16384000; i++)
+	{
 		tree->insert(test[i]);
+	}
 	
 	BPlusTreeNode * root = tree->getroot();
 	int rootnum = root->index_num;
 	for (int i = 0; i < rootnum; i++)
 		cout << root->indexs[i] << " ";
 	cout << endl;
+	system("pause");
 	return 0;
 }
