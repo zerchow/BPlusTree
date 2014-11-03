81b72ce02ebbf7288b13224d47b0af94af06808b
zerchow <455063040@qq.com>
2014-11-03 14:06:59 +0800

    final

--- BPlusTree/BPlusTree.h
+++ BPlusTree/BPlusTree.h
@@ -1,22 +1,20 @@
 #include <iostream>
 #include <cstring>
+#include <ctime>
+#include <cstdlib>
 using namespace std;
 
 #define M 2048
-#define L 5
+#define L 64
 //在内存中建立一个数据文件，文件大小必须在500M以上，具体数据项自行决定，项数20M以上;
-//假设项数20M = 20 * 1024 * 1024，L = 5;
-//则满树分支为2^22;
-//m^h = (2^11)^2;
-//则M = 2048，理想高度为2;
-class BPlusTreeBase
+class BPlusTreeBase //基类节点，派生内部节点和叶子节点;
 {
 public:
 	bool is_leaf;          //是否为叶子节点;
-	int index_num;         //索引数目;
+	short index_num;       //索引数目;
 };
 
-class  BPlusTreeNode : public BPlusTreeBase//B+树节点;
+class  BPlusTreeNode : public BPlusTreeBase//内部索引节点;
 {
 public:
 	int indexs[M];                  //索引;
@@ -27,13 +25,13 @@ public:
 struct Data
 {
 	int index;             //键值，充当索引;
-	char data[28];         //附属数据;
+	char data[24];         //附属数据;
 };
 
-class BPlusTreeData : public BPlusTreeBase
+class BPlusTreeData : public BPlusTreeBase//叶子数据节点;
 {
 public:
-	Data datas[L];         //数据;
+	Data * datas[L];       //数据指针;
 	BPlusTreeData * next;  //兄弟;
 	BPlusTreeNode * parent;//父母节点;
 };
@@ -41,15 +39,16 @@ public:
 class BPlusTree
 {
 private:
-	BPlusTreeNode * root;
-	BPlusTreeData * start;
-	BPlusTreeBase * find_leaf(int index)
+	BPlusTreeNode * root; //指向根节点的指针;
+	BPlusTreeData * start;//指向叶节点的指针;
+
+	BPlusTreeBase * find_leaf(int index) //插入时需要插入到叶子，首先要根据键值找到叶子;
 	{
-		BPlusTreeBase * finder = this->root;
+		BPlusTreeBase * finder = this->root;//从根节点开始找;
 		while(finder && !finder->is_leaf)
 		{
-			int i;
-			for(i = 0; i < finder->index_num; i ++)
+			short i;
+			for(i = 0; i < finder->index_num; ++i)
 			{
 				if(((BPlusTreeNode*)finder)->indexs[i] >= index)
 				{
@@ -57,53 +56,59 @@ private:
 					break;
 				}
 			}
-			if (i == finder->index_num)
+			if (i == finder->index_num)//如果键值大于所有索引，则插入到最后一个索引;
 			{
 				finder = ((BPlusTreeNode*)finder)->pointers[i - 1];
 			}
 		}
 		return finder;
 	}
-	void split_leaf(BPlusTreeData *& old_node, Data data)
+
+	void split_leaf(BPlusTreeData * old_node, Data * data) //分裂叶子节点;
 	{
 		BPlusTreeData * new_node = new BPlusTreeData();
-		int old_count = old_node->index_num;
-		int mid_index = old_count / 2;
+		short old_count = old_node->index_num;
+		short mid_index = old_count >> 1;
 		new_node->is_leaf = true;
 		new_node->next = old_node->next;
 		old_node->next = new_node;
 		new_node->parent = old_node->parent;
-		if (data.index < old_node->datas[mid_index].index)
+		/*
+		* 根据所要插入键值相对中间值的大小，分裂一个叶子节点;
+		* 顺序如下：处理新叶子节点 -> 把新叶子节点插入到父母内部节点 -> 将还未插入的键值进行插入;
+		*/
+		if (data->index < old_node->datas[mid_index]->index)
 		{
 			old_node->index_num = mid_index;
-			new_node->index_num = old_count - mid_index;
-			for (int i = 0; i < new_node->index_num; i++)
+			new_node->index_num = old_count - old_node->index_num;
+			for (short i = 0; i < new_node->index_num; ++i)
 			{
 				new_node->datas[i] = old_node->datas[i + mid_index];
 			}
 			this->fix_leaf_parent(old_node);
-			this->insert_in_node(new_node->parent, new_node->datas[new_node->index_num - 1].index, new_node);
+			this->insert_in_node(new_node->parent, new_node->datas[new_node->index_num - 1]->index, new_node);
 			this->insert_in_leaf(old_node, data);
 		}
 		else
 		{
 			old_node->index_num = mid_index + 1;
-			new_node->index_num = old_count - mid_index - 1;
-			for (int i = 0; i < new_node->index_num; i++)
+			new_node->index_num = old_count - old_node->index_num;
+			for (short i = 0; i < new_node->index_num; ++i)
 			{
 				new_node->datas[i] = old_node->datas[i + mid_index + 1];
 			}
 			this->fix_leaf_parent(old_node);
-			this->insert_in_node(new_node->parent, new_node->datas[new_node->index_num - 1].index, new_node);
+			this->insert_in_node(new_node->parent, new_node->datas[new_node->index_num - 1]->index, new_node);
 			this->insert_in_leaf(new_node, data);
 		}
 	}
-	void split_node(BPlusTreeNode *& old_node, int index, BPlusTreeBase * child)
+
+	void split_node(BPlusTreeNode * old_node, int index, BPlusTreeBase * child) //分裂内部节点;
 	{
 		BPlusTreeNode * new_node = new BPlusTreeNode();
-		int old_count = old_node->index_num;
-		int mid_index = old_count / 2;
-		if(old_node->parent == NULL)
+		short old_count = old_node->index_num;
+		short mid_index = old_count >> 1;
+		if(old_node->parent == NULL) //若父母为NULL，则说明此次要分裂的是根节点;
 		{
 			this->root = new BPlusTreeNode();
 			this->root->is_leaf = false;
@@ -115,17 +120,22 @@ private:
 		}
 		new_node->is_leaf = false;
 		new_node->parent = old_node->parent;
+		/*
+		* 根据所要插入键值相对中间值的大小，分裂一个内部节点;
+		* 顺序如下：处理新内部节点 -> 把新内部节点插入到父母内部节点 -> 将还未插入的键值进行插入;
+		*/
 		if (index < old_node->indexs[mid_index])
 		{
 			old_node->index_num = mid_index;
-			new_node->index_num = old_count - mid_index;
-			for (int i = 0; i < new_node->index_num; i++)
+			new_node->index_num = old_count - old_node->index_num;
+			for (short i = 0; i < new_node->index_num; ++i)
 			{
 				new_node->indexs[i] = old_node->indexs[i + mid_index];
 				new_node->pointers[i] = old_node->pointers[i + mid_index];
 			}
 			this->fix_node_parent(old_node);
 			this->insert_in_node(new_node->parent, new_node->indexs[new_node->index_num - 1], new_node);
+			//更改父母指针，如果不改，则会生成不正确的树;
 			if (child->is_leaf)
 			{
 				((BPlusTreeData*)child)->parent = old_node;
@@ -139,14 +149,15 @@ private:
 		else
 		{
 			old_node->index_num = mid_index + 1;
-			new_node->index_num = old_count - mid_index - 1;
-			for (int i = 0; i < new_node->index_num; i++)
+			new_node->index_num = old_count - old_node->index_num;
+			for (short i = 0; i < new_node->index_num; ++i)
 			{
 				new_node->indexs[i] = old_node->indexs[i + mid_index + 1];
 				new_node->pointers[i] = old_node->pointers[i + mid_index + 1];
 			}
 			this->fix_node_parent(old_node);
 			this->insert_in_node(new_node->parent, new_node->indexs[new_node->index_num - 1], new_node);
+			//更改父母指针，如果不改，则会生成不正确的树;
 			if (child->is_leaf)
 			{
 				((BPlusTreeData*)child)->parent = new_node;
@@ -158,18 +169,19 @@ private:
 			this->insert_in_node(new_node, index, child);
 		}
 	}
-	void insert_in_leaf(BPlusTreeData *& leaf, Data data)
+
+	void insert_in_leaf(BPlusTreeData * leaf, Data * data) //将数据指针插入到叶子节点;
 	{
-		if (leaf->index_num == L)
+		if (leaf->index_num == L) //如果叶子节点数据指针已经达到最大数目，则先分裂，再插入;
 		{
 			this->split_leaf(leaf, data);
 		}
-		else
+		else //否则通过数组的处理进行插入;
 		{
-			int i;
-			for (i = leaf->index_num - 1; i >= 0; i--)
+			short i;
+			for (i = leaf->index_num - 1; i >= 0; --i)
 			{
-				if (leaf->datas[i].index > data.index)
+				if (leaf->datas[i]->index > data->index)
 					leaf->datas[i + 1] = leaf->datas[i];
 				else
 					break;
@@ -179,16 +191,17 @@ private:
 			this->fix_leaf_parent(leaf);
 		}
 	}
-	void insert_in_node(BPlusTreeNode *& node, int index, BPlusTreeBase * child)
+	void insert_in_node(BPlusTreeNode * node, int index, BPlusTreeBase * child)
+	//将作为孩子节点的叶子节点或内部节点插入到父母节点;
 	{
-		if (node->index_num == M)
+		if (node->index_num == M) //如果父母节点数据指针已经达到最大数目，则先分裂，再插入;
 		{
 			this->split_node(node, index, child);
 		}
-		else
+		else //否则通过数组的处理进行插入;
 		{
-			int i;
-			for (i = node->index_num - 1; i >= 0; i--)
+			short i;
+			for (i = node->index_num - 1; i >= 0; --i)
 			{
 				if (node->indexs[i] > index)
 				{
@@ -204,34 +217,76 @@ private:
 			this->fix_node_parent(node);
 		}
 	}
-	void fix_leaf_parent(BPlusTreeData *& actual_leaf)
+
+	void fix_leaf_parent(BPlusTreeData * actual_leaf) //叶子节点改变，父母的索引要根据键值进行修复;
 	{
-		for (int i = 0; i < actual_leaf->parent->index_num; i++)
+		//1:二分查找（平均情况下比较快）;
+		/*int start_index = 0;
+		int end_index = actual_leaf->index_num;
+		int mid_index;
+		while(start_index < end_index)
 		{
-			if ((actual_leaf->parent)->pointers[i] == actual_leaf)
+			mid_index = (start_index + end_index) >> 1;
+			if(actual_leaf->parent->pointers[mid_index] == actual_leaf)
 			{
-				(actual_leaf->parent)->indexs[i] = actual_leaf->datas[actual_leaf->index_num - 1].index;
+				actual_leaf->parent->indexs[mid_index] = actual_leaf->datas[actual_leaf->index_num - 1]->index;
+				this->fix_node_parent(actual_leaf->parent);
+				break;
+			}
+			else if(actual_leaf->parent->pointers[mid_index] > actual_leaf)
+				end_index = mid_index;
+			else
+				start_index = mid_index + 1;
+		}*/
+		//2:顺序查找（平均查找速度较慢）;
+		for (short i = 0; i < actual_leaf->parent->index_num; ++i)
+		{
+			if (actual_leaf->parent->pointers[i] == actual_leaf)
+			{
+				actual_leaf->parent->indexs[i] = actual_leaf->datas[actual_leaf->index_num - 1]->index;
 				this->fix_node_parent(actual_leaf->parent);
 				break;
 			}
 		}
 	}
-	void fix_node_parent(BPlusTreeNode *& actual_node)
+
+	void fix_node_parent(BPlusTreeNode * actual_node) //孩子节点索引改变，父母索引要相应进行修复;
 	{
 		if (actual_node->parent == NULL)
 		{
 			return;
 		}
-		for (int i = 0; i < actual_node->parent->index_num; i++)
+		//1:二分查找（平均情况下比较快）;
+		/*int start_index = 0;
+		int end_index = actual_node->index_num;
+		int mid_index;
+		while(start_index < end_index)
 		{
-			if ((actual_node->parent)->pointers[i] == actual_node)
+		mid_index = (start_index + end_index) >> 1;
+		if(actual_node->parent->pointers[mid_index] == actual_node)
+		{
+		actual_node->parent->indexs[mid_index] = actual_node->indexs[actual_node->index_num - 1];
+		this->fix_node_parent(actual_node->parent);
+		break;
+		}
+		else if(actual_node->parent->pointers[mid_index] > actual_node)
+		end_index = mid_index;
+		else
+		start_index = mid_index + 1;
+		}*/
+		//2:顺序查找（平均查找速度较慢）;
+		for (short i = 0; i < actual_node->parent->index_num; ++i)
+		{
+			if (actual_node->parent->pointers[i] == actual_node)
 			{
-				(actual_node->parent)->indexs[i] = actual_node->indexs[actual_node->index_num - 1];
-				this->fix_node_parent(actual_node->parent);
+				actual_node->parent->indexs[i] = actual_node->indexs[actual_node->index_num - 1];
+				if(actual_node->parent != NULL)
+					this->fix_node_parent(actual_node->parent);
 				break;
 			}
 		}
 	}
+
 public:
 	BPlusTree() //构造函数，初始化作为根节点的内部节点;
 	{
@@ -240,7 +295,7 @@ public:
 		this->root->index_num = 0;       //无存储;
 		this->root->parent = NULL;       //无父母;
 	}
-	bool insert(Data data) //插入;
+	bool insert(Data * data) //插入;
 	{
 		if (this->root->index_num == 0) //如果插入的是第一个数据，则新建一个节点进行存储，并进行相应的初始化工作;
 		{
@@ -250,7 +305,7 @@ public:
 			first_data_node->next = NULL;                         //无兄弟;
 			first_data_node->datas[0] = data;                     //存储数据;
 			first_data_node->index_num = 1;                       //仅有第一个数据;
-			this->root->indexs[0] = data.index;                   //定根的索引;
+			this->root->indexs[0] = data->index;                   //定根的索引;
 			this->root->pointers[0] = first_data_node;            //定根的孩子;
 			this->root->index_num = 1;                            //仅有第一个孩子;
 			this->start = first_data_node;                        //范围查询的起点（因为分裂趋势向右，所以起点以后不会变）;
@@ -258,27 +313,55 @@ public:
 		}
 
 		//第二次及以后的数据插入;
-		BPlusTreeData * leaf = (BPlusTreeData*)this->find_leaf(data.index); //找到放置数据的叶子;
+		BPlusTreeData * leaf = (BPlusTreeData*)this->find_leaf(data->index); //找到放置数据的叶子;
 		
 		//重复数据不予插入;
-		for(int i = 0; i < leaf->index_num; i ++)
-			if(((BPlusTreeData*)leaf)->datas[i].index == data.index)
+		//1:二分查找（平均情况下比较快）;
+		int start_index = 0;
+		int end_index = leaf->index_num;
+		int mid_index;
+		while(start_index < end_index)
+		{
+			mid_index = (start_index + end_index) >> 1;
+			if(leaf->datas[mid_index]->index == data->index)
 				return false;
+			else if(leaf->datas[mid_index]->index > data->index)
+				end_index = mid_index;
+			else
+				start_index = mid_index + 1;
+		}
+		//2:顺序查找（平均查找速度较慢）;
+		/*for(short i = 0; i < leaf->index_num; ++i)
+			if(leaf->datas[i]->index == data->index)
+				return false;*/
 
 		this->insert_in_leaf(leaf, data);//往叶子插入数据;
 		return true;
 	}
-	BPlusTreeNode * getroot()//just for TMD test
+
+	Data * query(int index)
+	{
+		BPlusTreeData * leaf = (BPlusTreeData*)this->find_leaf(index); //找到放置数据的叶子;
+		for(int i = 0; i < leaf->index_num; ++i)
+		{
+			if(leaf->datas[i]->index == index)
+				return leaf->datas[i];
+		}
+		return NULL;
+	}
+
+	BPlusTreeNode * getroot() //前期测试，获取B+树的根节点;
 	{
 		return this->root;
 	}
-	int getheight()//just for TMD test
+
+	int getheight()//前期测试，获取B+树的高度;
 	{
 		BPlusTreeBase * height_calculator = this->root;
 		int height = 0;
 		while (height_calculator)
 		{
-			height++;
+			++height;
 			if (height_calculator->is_leaf)
 				break;
 			else
